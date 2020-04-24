2ebe030a22b41d0a28bcca8deace13ddc2305407
Grzegorz Poreba <grzegorz.poreba.73@gmail.com>
2020-04-24 13:19:58 +0200

    Updates

---
+++ precache-manifest.f87ac191013e3a3ae7d4b75a9747540b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9617837b46a15554aef27dc3c269c60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45698c8549bc17d92c1c",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "27ba10bb94e968626d8d",
+    "url": "/static/css/main.bb727509.chunk.css"
+  },
+  {
+    "revision": "45698c8549bc17d92c1c",
+    "url": "/static/js/2.218213f3.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.218213f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27ba10bb94e968626d8d",
+    "url": "/static/js/main.61986876.chunk.js"
+  },
+  {
+    "revision": "4f0d62aca3d666ecd58f",
+    "url": "/static/js/runtime-main.3b3db484.js"
+  },
+  {
+    "revision": "79d1084651676caea05ae844ccdd4a15",
+    "url": "/static/media/empytable.79d10846.jpg"
+  }
+]);
